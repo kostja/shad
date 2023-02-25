5328f3a35a9b4e06521428d157d856cfb528a19c
Sergey Rusakov <srusakov@fhmp0pn7h368ktmkk0nv.auto.internal>
2023-02-25 00:03:13 +0000

    Changed as much as I could into pure SQL.

--- srusakov/sql/init.sql
+++ srusakov/sql/init.sql
@@ -6,24 +6,13 @@ CREATE TABLE workers (
 
 COPY workers (id, parent_id, "name") from '/var/lib/postgresql/data/graph/graph.csv' CSV;
 
--- Correcting current value of the primary key sequence.
 SELECT setval('workers_id_seq', (SELECT MAX(id) FROM workers));
 
 ---------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------
 
--- 1) Procedure for worker addition.
+-- 1) Добавляем работника.
 CREATE OR REPLACE FUNCTION add_worker(parent_id INT, "name" TEXT)
-RETURNS void AS
-$BODY$
-BEGIN
-    INSERT INTO workers VALUES (nextval('workers_id_seq'), add_worker.parent_id, add_worker.name);
-    RAISE NOTICE 'Added worker % with id % and parent_id %', "name", currval('workers_id_seq'), parent_id;
-END;
-$BODY$
-LANGUAGE 'plpgsql';
-
-CREATE OR REPLACE FUNCTION sql_add_worker(parent_id INT, "name" TEXT)
 RETURNS void AS
 $BODY$
     INSERT INTO workers VALUES (nextval('workers_id_seq'), sql_add_worker.parent_id, sql_add_worker.name);
@@ -31,52 +20,31 @@ $BODY$
 LANGUAGE SQL;
     
 
--- 2) Procedure for changing the "department" of the worker. Since we store
--- everything in one table this translates to simply changing the parent of the
--- worker.
-CREATE FUNCTION change_worker_parent(worker_id INT, new_parent_id INT)
+-- 2) Меняем отдел работника, что в данной структуре соответствует просто смене родителя.
+CREATE OR REPLACE FUNCTION change_worker_parent(worker_id INT, new_parent_id INT)
 RETURNS void AS
 $BODY$
-BEGIN
     UPDATE workers SET workers.parent_id = new_parent_id WHERE workers.id = worker_id;
-    RAISE NOTICE 'Changed parent to % for worker %', new_parent_id, worker_id;
-END;
 $BODY$
-LANGUAGE 'plpgsql';
+LANGUAGE SQL;
 
--- 3) Printing out workers from the "department", which is determined by head's id.
-CREATE FUNCTION print_dpt_workers(dpt_id INT)
-RETURNS void AS 
+-- 3) Собираем работников отдела, определяющегося айдишником руководителя.
+CREATE OR REPLACE FUNCTION dpt_workers(dpt_id INT)
+RETURNS TABLE("name" TEXT, id INT) AS 
 $BODY$
-DECLARE
-    parent_name TEXT;
-    parent_id INT;
-    len INT;
-    worker_names TEXT[];
-    worker_ids INT[];
-BEGIN
-    SELECT workers.name, workers.id INTO parent_name, parent_id FROM workers WHERE workers.id = dpt_id;
-    RAISE INFO 'Department head is %(%)', parent_name, parent_id;
-
-    worker_names := ARRAY(SELECT workers.name FROM workers WHERE workers.parent_id = dpt_id);
-    worker_ids := ARRAY(SELECT workers.id FROM workers WHERE workers.parent_id = dpt_id);
-    len := array_length(worker_names, 1);
-    RAISE INFO 'Workers are:';
-    WHILE len > 0 LOOP
-        RAISE INFO '%(%)', worker_names[len], worker_ids[len];
-        len := len - 1;
-    END LOOP;
-END;
+    SELECT 
+        workers.name, workers.id 
+    FROM 
+        workers 
+    WHERE 
+        workers.id = dpt_id OR workers.parent_id = dpt_id;
 $BODY$
-LANGUAGE 'plpgsql';
+LANGUAGE SQL;
 
--- 4) Returning a table with all workers lacking subordinates. Here it makes sense to specifically return a table
--- instead of printing since there is quite a number of leaf workers.
-CREATE FUNCTION leaf_workers()
+-- 4) Возвращаем табличку с работниками без подчиненных.
+CREATE OR REPLACE FUNCTION leaf_workers()
 RETURNS TABLE(id INT, "name" TEXT) AS
 $BODY$
-BEGIN
-    RETURN QUERY
     WITH worker_ids_with_subs AS (
         SELECT DISTINCT ON (workers.parent_id)
             workers.parent_id AS id 
@@ -96,13 +64,11 @@ BEGIN
         WHERE 
             workers.id = worker_ids_with_subs.id
     );
-END;
 $BODY$
-LANGUAGE 'plpgsql';
+LANGUAGE SQL;
 
--- 5) Finding a subordination chain for an worker - a sequence of bosses all the way up to -1.
--- Simple recursive approach with exit condition on parent_id == -1.
-CREATE FUNCTION print_command_chain(worker_id INT)
+-- 5) Печатаем цепочку подчинения.
+CREATE OR REPLACE FUNCTION print_command_chain(worker_id INT)
 RETURNS void AS
 $BODY$
 DECLARE
@@ -125,41 +91,16 @@ BEGIN
 
     RAISE INFO '%(%)', worker_name, worker_id;
     IF parent_id <> -1 THEN
-        PERFORM print_command_chain(parent_id); -- PERFORM is needed because of the void return type.
+        PERFORM print_command_chain(parent_id); -- PERFORM потому что void.
     END IF;
 END;
 $BODY$
 LANGUAGE 'plpgsql';
 
--- 6.a) Printing out the amount of people in the department + all subordinate departments. Simply calling 
--- the function recursively for each subordinate worker in a department and aggregating the result.
-CREATE FUNCTION dpt_size(dpt_id INT)
-RETURNS INT AS
-$BODY$
-DECLARE
-    amount INT;
-    len INT;
-    sub_worker_ids INT[];
-BEGIN
-    amount := 1;
-    sub_worker_ids := ARRAY(SELECT workers.id FROM workers WHERE workers.parent_id = dpt_id);
-    len := array_length(sub_worker_ids, 1);
-    WHILE len > 0 LOOP
-        amount := amount + dpt_size(sub_worker_ids[len]);
-        len := len - 1;
-    END LOOP;
-    RETURN amount;
-END;
-$BODY$
-LANGUAGE 'plpgsql';
-
-
--- 6.b) Returning a table with people in the department + all subordinate departments. Recursive CTE solution here.
-CREATE FUNCTION dpt_workers_rec(dpt_id INT)
+-- 6) Возвращаем табличку со всеми работниками отдела, включая подотделы.
+CREATE OR REPLACE FUNCTION dpt_workers_rec(dpt_id INT)
 RETURNS TABLE(id INT, parent_id INT, "name" TEXT) AS
 $BODY$
-BEGIN
-    RETURN QUERY
     WITH RECURSIVE dpt_workers AS (
 	SELECT
 		workers.id,
@@ -181,12 +122,12 @@ BEGIN
         *
     FROM
         dpt_workers;
-END;
 $BODY$
-LANGUAGE 'plpgsql';
+LANGUAGE SQL;
 
--- 7) Checking for 2 anomalies - invalid parent and several trees. No check for cycles :(
-CREATE FUNCTION check_for_anomalies()
+-- 7) Проверяем 2 аномалии - плохие родители и больше чем одну компоненту. Циклы не осилил, хотя
+-- успешная проверка на компоненту в том виде, в котором она здесь сделана говорит, что и циклов нет.
+CREATE OR REPLACE FUNCTION check_for_anomalies()
 RETURNS void AS
 $BODY$
 DECLARE
@@ -198,7 +139,7 @@ BEGIN
     INTO num_workers
     FROM workers;
 
-    -- Checking invalid parent relations with SELECT *** WHERE NOT EXISTS (select for parent in workers).
+    -- Проверяем плохих родителей с помощью SELECT *** WHERE NOT EXISTS (select for parent in workers).
     invalid_parent_ids := ARRAY(
         SELECT 
             l_workers.parent_id
@@ -210,13 +151,13 @@ BEGIN
             r_workers.id IS NULL
     );
 
-    IF array_length(invalid_parent_ids, 1) <> 1 THEN -- one person has parent_id == -1
+    IF array_length(invalid_parent_ids, 1) <> 1 THEN -- один работник имеет -1 родителя
         RAISE INFO 'ANOMALY - there are bad parent ids: %', invalid_parent_ids;
     ELSE
         RAISE INFo 'OK - parent ids';
     END IF;
 
-    -- Checking for several trees by seeing how many workers we can reach from worker with id 1.
+    -- Проверяем на компоненту, считая сколько людей можно достичь из работника с id = 1.
     WITH RECURSIVE reached_workers AS (
         SELECT
             workers.id,
@@ -249,13 +190,9 @@ $BODY$
 LANGUAGE 'plpgsql';
 
 
--- 8) Print out the rank of the worker - number of levels below the worker, including his own.
--- Calculating the rank by iteratively trying to add all of the subordinates to temp table, so on
--- the first iteration we add people directly subordinate to the worker and then we start to add people,
--- which are subordinate to the added ones as well. When table stops changing, it means that we reached
--- all the leafs and rank then is the amount of iteration done.
--- PS: number of bosses above the worker can be obtained from 5) with minimal changes.
-CREATE FUNCTION print_rank(worker_id INT)
+-- 8) Печатаем ранг работника - итеративно добавляем подвластных работников в табличку пока
+-- не обнаруживаем, что никого больше не добавили, после чего выводим ранг как кол-во итераций.
+CREATE OR REPLACE FUNCTION print_rank(worker_id INT)
 RETURNS void AS
 $BODY$
 DECLARE
@@ -267,18 +204,18 @@ BEGIN
     rank := 1;    
 
     CREATE TEMP TABLE IF NOT EXISTS agg_subs (id SERIAL PRIMARY KEY);
-    TRUNCATE TABLE agg_subs; -- cleaning the table between invocations
-    -- "INSERT INTO" specifically so agg_subs is correctly processed in function scope.
+    TRUNCATE TABLE agg_subs; -- чистим между вызовами
+    -- "INSERT INTO" для того, чтобы не было жалоб из-за скоупа.
     INSERT INTO agg_subs VALUES (worker_id); 
     prev_count := 1;
     cur_count := 1;
     count_changed := true;
 
-    -- While loop instead of recursive CTE, because I couldn't think of a way to increment rank and define a
-    -- stopping condition in a CTE.
+    -- Итеарация вместо рекурсивного CTE, потому что не осилил как вставить нормальное условие
+    -- на остановку.
     WHILE count_changed LOOP
-        RAISE INFO 'Rank is at least %', rank; -- left in to provide at least some feel for the speed of iteration.
-        -- SELECT INTO has different semantics in plpgsql, so using INSERT INTO.
+        RAISE INFO 'Rank is at least %', rank; -- оставил, чтобы можно было хоть немного понимать работает или нет.
+        -- SELECT INTO отличается по функционалу в plpgsql, так что INSERT INTO.
         INSERT INTO agg_subs
         SELECT
             workers.id
@@ -294,9 +231,9 @@ BEGIN
         SELECT count(*) INTO cur_count FROM agg_subs;
 
         IF cur_count = prev_count THEN
-            count_changed := false; -- exiting the loop
+            count_changed := false; -- выходим из цикла
         ELSE
-            rank := rank + 1; -- increasing the rank
+            rank := rank + 1;
             prev_count := cur_count;
         END IF;
     END LOOP;
@@ -306,19 +243,44 @@ END;
 $BODY$
 LANGUAGE 'plpgsql';
 
-/*
--- 9) 
-CREATE FUNCTION fn()
-RETURNS *** AS
+-- 9) Печатаем иерархию отдела. Здесь концептульано даже не понимаю, как сделать это на
+-- чистом SQL, потому что надо что-то содержательное печатать. Но выполняется, конечно, миллиард лет :(
+CREATE OR REPLACE FUNCTION print_hierarchy(dpt_id INT, depth INT DEFAULT 0)
+RETURNS void AS
 $BODY$
+DECLARE
+prefix TEXT;
+len INT;
+cur_name TEXT;
+cur_worker_ids INT[];
+i INT;
 BEGIN
-    
+    prefix := '';
+    i := 0;
+    WHILE i < depth LOOP
+        prefix := prefix || '  ';
+        i := i + 1;
+    END LOOP;
+
+    SELECT "name" INTO cur_name FROM workers WHERE workers.id = dpt_id;
+
+    RAISE INFO '% %', prefix, cur_name;
+
+    cur_worker_ids := ARRAY(
+        SELECT workers.id FROM workers WHERE workers.parent_id = dpt_id
+    );
+
+    len := array_length(cur_worker_ids, 1);
+    WHILE len > 0 AND len IS NOT NULL LOOP
+        PERFORM print_hierarchy(cur_worker_ids[len], depth + 1);
+        len := len - 1;
+    END LOOP;
 END;
 $BODY$
 LANGUAGE 'plpgsql';
-*/
 
-CREATE OR REPLACE FUNCTION paths(src_id INT, dst_id INT)
+-- 10) Печатаем наикратчайший путь между двумя работниками.
+CREATE OR REPLACE FUNCTION print_shortest_path(src_id INT, dst_id INT)
 RETURNS void AS
 $BODY$
 DECLARE
@@ -333,6 +295,7 @@ BEGIN
     TRUNCATE TABLE reached_workers;
     INSERT INTO reached_workers VALUES (-1, src_id);
 
+    -- Это просто поиск в ширину.
     WHILE NOT dst_reached LOOP
         INSERT INTO reached_workers
         SELECT
@@ -344,9 +307,9 @@ BEGIN
             reached_workers
         INNER JOIN workers 
         ON 
-            reached_workers.id = workers.parent_id -- reaching child
+            reached_workers.id = workers.parent_id -- можем достичь ребенка
             OR 
-            reached_workers.id = workers.id -- reaching parent
+            reached_workers.id = workers.id -- можем достичь предка
         ON CONFLICT
             DO NOTHING;
 
@@ -355,8 +318,8 @@ BEGIN
         END IF;
     END LOOP;
 
-    -- Using arrays allows to get a deteminitsin ordering in contrast to joins in recursive
-    -- CTE, where ordering is not enforced(?)
+    -- Здесь массив, чтобы обеспечить правильный порядок. Джойны вроде не обязаны соблюдать
+    -- какие-либо порядки строк(?).
     WHILE path_ids[array_length(path_ids, 1)] <> src_id LOOP
         array_append(
             path_ids, 
@@ -377,83 +340,3 @@ BEGIN
 END;
 $BODY$
 LANGUAGE 'plpgsql';
-
--- 10) Print out the path between 2 workers, defined by their ids.
--- To do this we effectively treat our tree as bidirected graph and doing breadth first search by iteratively
--- appending ids, which we can reach to the ones that we reached until we meet dst_id in a way similar to 7).
-CREATE FUNCTION print_shortest_path(src_id INT, dst_id INT)
-RETURNS void AS
-$BODY$
-DECLARE
-len INT;
-path_id_arr INT[];
-BEGIN
-    path_ids := ARRAY(
-        WITH RECURSIVE reached_workers AS (
-            SELECT
-                -1 AS prev_id,
-                workers.id AS id
-            FROM
-                workers
-            WHERE
-                workers.id = src_id
-            UNION
-                SELECT 
-                    (CASE WHEN reached_workers.id = workers.parent_id
-                        THEN workers.parent_id ELSE reached_workers.id END) AS prev_id,
-                    (CASE WHEN reached_workers.id = workers.parent_id
-                        THEN workers.id ELSE workers.parent_id END) AS id
-                FROM
-                    workers
-                INNER JOIN reached_workers 
-                ON 
-                    (reached_workers.id = workers.parent_id OR reached_workers.id = workers.id) 
-                    AND 
-                    (reached_workers.prev_id != workers.id)
-                WHERE dst_id NOT IN (reached_workers.id)
-        ), path_ids AS (
-            SELECT
-                dst_id as id
-            FROM 
-                reached_workers
-            UNION
-                SELECT 
-                    reached_workers.prev_id AS id
-                FROM 
-                    reached_workers
-                INNER JOIN path_ids ON path_ids.id = reached_workers.id
-                WHERE src_id NOT IN (path_ids.id)
-        ) SELECT * FROM path_ids
-    );
-
-    len := array_length(path_id_arr, 1);
-    WHILE len > 0 LOOP
-        RAISE INFO '%', path_id_arr[len];
-    END LOOP;
-END;
-$BODY$
-LANGUAGE 'plpgsql';
-
-WITH RECURSIVE reached_workers AS (
-    SELECT
-        -1 AS prev_id,
-        workers.id AS id
-    FROM
-        workers
-    WHERE
-        workers.id = 11
-    UNION
-        SELECT
-            (CASE WHEN reached_workers.id = workers.parent_id
-                THEN reached_workers.id ELSE reached_workers.id END) AS prev_id,
-            (CASE WHEN reached_workers.id = workers.parent_id
-                THEN workers.id ELSE workers.parent_id END) AS id
-        FROM
-            reached_workers
-        INNER JOIN workers 
-        ON 
-            reached_workers.id = workers.parent_id -- reaching child
-            OR 
-            reached_workers.id = workers.id -- reaching parent
-        WHERE 9 NOT IN (reached_workers.id)
-) select * from reached_workers;
